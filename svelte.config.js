eed71878db50f71146f75055b71050daf94cbf0c
adam <adam@augmenteddestiny.com>
2020-12-11 23:20:55 -0600

    Forgot to push?

--- svelte.config.js
+++ svelte.config.js
@@ -2,22 +2,18 @@ const sveltePreprocess = require("svelte-preprocess");
 const postcss = require("./postcss.config");
 
 const createPreprocessors = ({ sourceMap }) => [
-	sveltePreprocess({
-		sourceMap,
-		defaults: {
-			script: "typescript",
-			style: "postcss",
-		},
-		scss: {
-      includePaths: ['./src/theme'],
+  sveltePreprocess({
+    sourceMap,
+    scss: {
+      includePaths: ["./src/theme"],
     },
-		postcss,
-	}),
-	// You could have more preprocessors, like mdsvex
+    postcss,
+  }),
+  // You could have more preprocessors, like mdsvex
 ];
 
 module.exports = {
-	createPreprocessors,
-	// Options for `svelte-check` and the VS Code extension
-	preprocess: createPreprocessors({ sourceMap: true }),
+  createPreprocessors,
+  // Options for `svelte-check` and the VS Code extension
+  preprocess: createPreprocessors({ sourceMap: true }),
 };
